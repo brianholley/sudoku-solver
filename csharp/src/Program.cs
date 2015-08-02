d3f4d8d85d4b58ca4f08b8e264632aea9e47814e
Brian Holley <brian.holley@hotmail.com>
2015-08-01 18:53:07 -0700

    Deserialize puzzle from string with unit tests. Extract row and calculate possibilities.

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace SudokuSolver
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SudokuSolver
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+		}
+
+		public static IEnumerable<int> Puzzle(string input)
+		{
+			return input.ToCharArray(0, 81).Select(c => (c == '.' ? -1 : int.Parse(c + "")));
+		}
+
+		public static IEnumerable<int> Row(IEnumerable<int> puzzle, int rowIndex)
+		{
+			int rowStart = rowIndex - (rowIndex % 9);
+			return puzzle.Skip(rowStart).Take(9);
+		}
+
+		public static IEnumerable<int> Possibilities(IEnumerable<int> row, IEnumerable<int> column, IEnumerable<int> square)
+		{
+			var used = new HashSet<int>(row.Concat(column).Concat(square));
+			used.Remove(-1);
+			return Enumerable.Range(1, 9).Except(used);
 		}
 	}
 }
--- csharp/unittests/Tests.cs
+++ csharp/unittests/Tests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuSolver;
 
 namespace UnitTests
 {
@@ -7,8 +9,43 @@ namespace UnitTests
 	public class Tests
 	{
 		[TestMethod]
-		public void TestMethod1()
+		public void Puzzle_Filler()
 		{
+			var puzzle = Program.Puzzle(new string('1', 81));
+			Assert.AreEqual(81, puzzle.Count());
+			Assert.AreEqual(1, puzzle.First());
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(FormatException))]
+		public void Puzzle_InvalidCharacters()
+		{
+			var puzzle = Program.Puzzle(new string('-', 81));
+			puzzle.First();
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentOutOfRangeException))]
+		public void Puzzle_InvalidTooShort()
+		{
+			var puzzle = Program.Puzzle(new string('1', 80));
+			puzzle.First();
+		}
+
+		[TestMethod]
+		public void Puzzle_InvalidTooLong()
+		{
+			var puzzle = Program.Puzzle(new string('1', 82));
+			Assert.AreEqual(81, puzzle.Count());
+		}
+
+		[TestMethod]
+		public void Puzzle_ValidIncomplete()
+		{
+			var puzzle = Program.Puzzle(".19.6..343.8.51...64...3.......194..47.8.5.19..564.......5...68...17.9.398..3.57.");
+			Assert.AreEqual(81, puzzle.Count());
+			Assert.AreEqual(-1, puzzle.First());
+			Assert.AreEqual(9, puzzle.Skip(2).First());
 		}
 	}
 }
