d209b40bf93434753b67f70cf78a028462a19042
Brian Holley <brian.holley@hotmail.com>
2015-08-02 18:55:29 -0700

    Extract columns and squares with unit tests

--- csharp/src/Program.cs
+++ csharp/src/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SudokuSolver
@@ -7,6 +8,13 @@ namespace SudokuSolver
 	{
 		static void Main(string[] args)
 		{
+			if (args.Length != 1)
+			{
+				Console.Out.WriteLine("Usage: sodoku-solver-csharp <puzzle>");
+				Console.Out.WriteLine("Use dots for empty spaces, e.g. 12..5.78.");
+				return;
+			}
+			Puzzle(args[0]);
 		}
 
 		public static IEnumerable<int> Puzzle(string input)
@@ -14,10 +22,22 @@ namespace SudokuSolver
 			return input.ToCharArray(0, 81).Select(c => (c == '.' ? -1 : int.Parse(c + "")));
 		}
 
-		public static IEnumerable<int> Row(IEnumerable<int> puzzle, int rowIndex)
+		public static IEnumerable<int> Row(IEnumerable<int> puzzle, int index)
 		{
-			int rowStart = rowIndex - (rowIndex % 9);
+			int rowStart = index - (index % 9);
 			return puzzle.Skip(rowStart).Take(9);
+		}
+
+		public static IEnumerable<int> Column(IEnumerable<int> puzzle, int index)
+		{
+			int column = index % 9;
+			return puzzle.Where((v, i) => (i % 9 == column));
+		}
+
+		public static IEnumerable<int> Square(IEnumerable<int> puzzle, int index)
+		{
+			int square = (index % 9) / 3 + ((index / 9) / 3) * 3;
+			return puzzle.Where((v, i) => ((i % 9) / 3 + ((i / 9) / 3) * 3 == square));
 		}
 
 		public static IEnumerable<int> Possibilities(IEnumerable<int> row, IEnumerable<int> column, IEnumerable<int> square)
--- csharp/unittests/Tests.cs
+++ csharp/unittests/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SudokuSolver;
@@ -46,6 +47,55 @@ namespace UnitTests
 			Assert.AreEqual(81, puzzle.Count());
 			Assert.AreEqual(-1, puzzle.First());
 			Assert.AreEqual(9, puzzle.Skip(2).First());
+		}
+
+		[TestMethod]
+		public void Row_Valid_StartingRowIndex()
+		{
+			var puzzle = Program.Puzzle(".19.6..343.8.51...64...3.......194..47.8.5.19..564.......5...68...17.9.398..3.57.");
+			var row = Program.Row(puzzle, 0);
+			Assert.IsTrue(new int[] {-1, 1, 9, -1, 6, -1, -1, 3, 4}.SequenceEqual(row.ToArray()));
+		}
+
+		[TestMethod]
+		public void Row_Valid_NonStartingRowIndex()
+		{
+			var puzzle = Program.Puzzle(".19.6..343.8.51...64...3.......194..47.8.5.19..564.......5...68...17.9.398..3.57.");
+			var row = Program.Row(puzzle, 14);
+			Assert.IsTrue(new int[] {3, -1, 8, -1, 5, 1, -1, -1, -1}.SequenceEqual(row.ToArray()));
+		}
+
+		[TestMethod]
+		public void Column_Valid_StartingColumnIndex()
+		{
+			var puzzle = Program.Puzzle(".19.6..343.8.51...64...3.......194..47.8.5.19..564.......5...68...17.9.398..3.57.");
+			var column = Program.Column(puzzle, 1);
+			Assert.IsTrue(new int[] {1, -1, 4, -1, 7, -1, -1, -1, 8}.SequenceEqual(column.ToArray()));
+		}
+
+		[TestMethod]
+		public void Square_Valid_StartingColumnIndex()
+		{
+			var puzzle = Program.Puzzle(".19.6..343.8.51...64...3.......194..47.8.5.19..564.......5...68...17.9.398..3.57.");
+			var square = Program.Square(puzzle, 1);
+			Assert.IsTrue(new int[] {-1, 1, 9, 3, -1, 8, 6, 4, -1}.SequenceEqual(square.ToArray()));
+		}
+
+		[TestMethod]
+		public void Possibilities_Empty()
+		{
+			var possibilities = Program.Possibilities(Enumerable.Repeat(-1, 9), Enumerable.Repeat(-1, 9), Enumerable.Repeat(-1, 9));
+			Assert.IsTrue(Enumerable.Range(1, 9).SequenceEqual(possibilities));
+		}
+
+		[TestMethod]
+		public void Possibilities_None()
+		{
+			var row = new int[] {1, 2, 3, -1, -1, -1, -1, -1, -1};
+			var column = new int[] {-1, -1, -1, 4, 5, 6, -1, -1, -1};
+			var square = new int[] {-1, -1, -1, -1, -1, -1, 7, 8, 9};
+			var possibilities = Program.Possibilities(row, column, square);
+			Assert.AreEqual(0, possibilities.Count());
 		}
 	}
 }
