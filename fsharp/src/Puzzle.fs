4cc58986fbece151665baf4664e771076894f92a
Brian Holley <brian.holley@hotmail.com>
2018-09-15 22:08:05 -0700

    Add row/column/square unit tests

--- fsharp/src/Puzzle.fs
+++ fsharp/src/Puzzle.fs
@@ -8,20 +8,22 @@ module Puzzle =
         let possibilities = possibilities
         new() = GridSquare(-1, list<int>.Empty)
 
-    let row (puzzle:int[], index:int) = 
+    let row (puzzle:list<int>) (index:int) : list<int> = 
         let rowStart = index - (index % 9)
-        puzzle.[rowStart..rowStart + 9]
+        puzzle.[rowStart..rowStart + 8]
 
-    let column (puzzle:int[], index:int) = 
+    let column (puzzle:list<int>) (index:int) : list<int> = 
         puzzle
             |> Seq.mapi (fun i v -> if i % 9 = index % 9 then Some(v) else None)
             |> Seq.choose id
+            |> Seq.toList
 
     let squareNumber (index:int) = 
         (index % 9) / 3 + ((index / 9) / 3) * 3
 
-    let square (puzzle:int[], index:int) = 
+    let square (puzzle:list<int>) (index:int) : list<int> = 
         let square = squareNumber index
         puzzle
             |> Seq.mapi (fun i v -> if squareNumber i  = square then Some(v) else None)
             |> Seq.choose id
+            |> Seq.toList
